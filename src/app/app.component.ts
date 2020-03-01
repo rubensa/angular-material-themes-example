ace9c4d34c17d86e40b253212995c21b8e55716e
Ruben Suarez <rubensa@gmail.com>
2020-03-01 00:13:12 +0000

    Fix Select component dinamically change theme
    Select is an special type of component
    It creates an overlay over the standard layout
    We need to import OverlayContainer and inject it
    Then we need to update OverlayContainer css class on theme change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'amte-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-material-themes-example';
   theme = 'light-theme';
+
+  constructor(private overlayContainer: OverlayContainer) { }
+
+  ngOnInit(): void {
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+    overlayContainerClasses.add(this.theme);
+  }
+
+  onThemeChange() {
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+    const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
+    if (themeClassesToRemove.length) {
+      overlayContainerClasses.remove(...themeClassesToRemove);
+    }
+    overlayContainerClasses.add(this.theme);
+  }
 }
